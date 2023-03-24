440d81be44c3dc8fe2f2bd5fff45b2b648d16ba4
ridwan1302210076 <127034652+ridwan1302210076@users.noreply.github.com>
2023-03-24 09:14:13 +0700

    add

--- Mod5_1302210076/Program.cs
+++ Mod5_1302210076/Program.cs
@@ -20,4 +20,33 @@ public class Penjumlahan<T>
         dynamic cc = c;
         Console.WriteLine(aa + bb + cc);
     }
+    public class SimpleDataBase<T>
+    {
+        List<T> storedData { get; set; }
+        List<DateTime> inputDates { get; set; }
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now.Date);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData.ElementAt(i) + ", yang disimpan pada waktu "
+                    + inputDates.ElementAt(i));
+            }
+        }
+    }
 }
+
+
+
